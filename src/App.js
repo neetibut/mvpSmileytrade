d8995492d5f891db573156b6b257a7b095ce509e
neetibut <62812751+neetibut@users.noreply.github.com>
2021-10-28 18:46:22 +0700

    mvpSmileytrade

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css';
+import React, { useState, useRef } from "react";
+import { ThemeProvider } from "styled-components";
+import { useOnClickOutside } from "./hooks";
+import { GlobalStyles } from "./global";
+import { theme } from "./theme";
+import { Burger, Menu } from "./components";
+import FocusLock from "react-focus-lock";
+import heroImage from "./images/1.png";
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  const menuId = "main-menu";
+
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World!
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div ref={node}>
+          <FocusLock disabled={!open}>
+            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+            <Menu open={open} setOpen={setOpen} id={menuId} />
+          </FocusLock>
+        </div>
+        <div>
+          <h2>
+            Welcome to your trading terminal <span aria-hidden="true">👋</span>
+          </h2>
+          <h1>Trade US stocks and Cryptocurrencies</h1>
+          <img src={heroImage} alt="smileytrade" />
+          <small>
+            Made with{" "}
+            <span aria-hidden="true" style={{ marginRight: ".5rem" }}>
+              ❤️
+            </span>
+            by <a href="https://www.teamxearth.com">teamxearth</a>
+          </small>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,6 @@
+export const theme = {
+  primaryDark: "rgb(0 122 255)",
+  primaryLight: "#EFFFFA",
+  primaryHover: "#343078",
+  mobile: "576px",
+};
